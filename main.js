08f8e6b55790ab346f9920b092d7d83e94bc006a
Amanda Harman <amanda.harman@willowtreeapps.com>
2018-12-31 16:59:14 -0500

    Show selection of elements

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ var selectedElements = [];
 const gridElement = document.querySelector('.grid');
 const addButton = document.getElementById('addButton');
 const deleteButton = document.getElementById('deleteButton');
+const deleteSelectedButton = document.getElementById('deleteSelectedButton');
 const jsonButton = document.getElementById('jsonButton');
-
 
 // Create a grid using the Muuri framework that allows drag and drop
 var grid = new Muuri('.grid', { dragEnabled: true});
@@ -23,8 +23,9 @@ main();
 function main() {
     addButton.addEventListener('click', addItem);
     deleteButton.addEventListener('click', removeItems);
+    deleteSelectedButton.addEventListener('click', removeSelectedItems, selectedElements);
     jsonButton.addEventListener('click', generateJSON);
-    gridElement.addEventListener('click', showSelection);
+    document.addEventListener('click', selectElement, event);
 }
 
 // Build the GridData object and convert it to a JSON string
@@ -37,13 +38,14 @@ function generateJSON() {
 
 // Add a grid item to DOM
 function addItem() {
-    var id = "1x1";
+    var id = gridElement.children.length + 1;
     var fragment = createDOMFragment(
         '<div class="item">' + 
             '<div class="item-content">' + id + '</div>' +
         '</div>');
     grid.add(fragment.firstChild);
     document.body.insertBefore(fragment, document.body.childNodes[0]);
+
 }
 
 // Create document fragment for given html string
@@ -66,7 +68,21 @@ function removeItems() {
     }   
 }
 
-function showSelection() {
-    // TODO: UI to show a box is selected
-    gridElement.style.color = "red";
+// Add selected item to array and show that it is selected
+function selectElement(event) {
+    for (i=0; i < gridElement.children.length; i++) {
+        if (event.target === gridElement.children[i].firstChild) {
+         selectedElements.push(gridElement.children[i]);
+         event.target.style.background = "red";
+        }
+    }
+}
+
+// TODO: Not working. Remove selected items from the grid
+function removeSelectedItems(selectedItems) {
+    grid.remove(selectedItems);
+    for (var i=0; i<selectedItems.length; i++) {
+        gridElement.removeChild(selectedItems[i].outterHTML);
+    }   
+    selectedItems = [];
 }
