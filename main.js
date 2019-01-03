a7058b9c7ebb4768517a5494183510ef54fcacb6
Amanda Harman <amanda.harman@willowtreeapps.com>
2019-01-03 11:48:27 -0500

    Generate basic JSON structure

--- main.js
+++ main.js
@@ -1,28 +1,60 @@
 // Container for data to be included in JSON file for config
 class GridData {
-    constructor(numberOfRows, numberOfColumns) {
-        this.numberOfRows = numberOfRows;
-        this.numberOfColumns = numberOfColumns;
+    constructor(title, titleVisible, layout, widgets) {
+        this.title = title;
+        this.titleVisible = titleVisible;
+        this.layout = layout;
+        this.widgets = widgets;
+    }
+}
+
+class ItemData {
+    constructor(col, row, width, height, widget, job, config) {
+        this.col = col;
+        this.row = row;
+        this.width = width;
+        this.height = height;
+        this.widget = widget, 
+        this.job = job;
+        this.config = config;
+    }
+}
+
+class Layout {
+    constructor(gridSize) {
+        this.gridSize = gridSize;
+    }
+}
+
+class GridSize {
+    constructor(columns, rows) {
+        this.columns = columns;
+        this.rows = rows;
     }
 }
 
 // Array to hold all visible grid elements? (Currently not being used)
 var selectedElements = [];
 
+const titleInput = document.getElementById("titleInput");
+const titleVisibleToggle = document.getElementById("titleVisibleToggle");
 const gridElement = document.querySelector('.grid');
 const addButton = document.getElementById('addButton');
 const deleteButton = document.getElementById('deleteButton');
 const deleteSelectedButton = document.getElementById('deleteSelectedButton');
 const jsonButton = document.getElementById('jsonButton');
+const jsonLabel = document.getElementById('jsonParagraph');
 
 // Create a grid using the Muuri framework that allows drag and drop
 var grid = new Muuri('.grid', {
     dragEnabled: true
 });
 
-main();
+setup();
 
-function main() {
+function setup() {
+    titleInput.placeholder = "Enter title";
+    titleVisibleToggle.addEventListener('click', toggleTitleVisibility);
     addButton.addEventListener('click', addItem);
     deleteButton.addEventListener('click', removeItems);
     deleteSelectedButton.addEventListener('click', removeSelectedItems);
@@ -30,12 +62,16 @@ function main() {
     document.addEventListener('click', toggleSelectElement);
 }
 
-// Build the GridData object and convert it to a JSON string
-function generateJSON() {
-    var layout = grid.layout;
-    var gridData = new GridData(2, 3);
-    var jsonString = JSON.stringify(gridData)
-    document.write(jsonString)
+/* Event Actions */
+
+// Toggle to change title visibility
+// TODO: Make a custom button with a state? 
+function toggleTitleVisibility() {
+    if (titleVisibleToggle.textContent === "Set Visible") {
+        titleVisibleToggle.textContent = "Set Hidden";
+    } else {
+        titleVisibleToggle.textContent = "Set Visible";
+    }
 }
 
 // Add a grid item to DOM
@@ -47,18 +83,6 @@ function addItem() {
         '</div>');
     grid.add(fragment.firstChild);
     document.body.insertBefore(fragment, document.body.childNodes[0]);
-
-}
-
-// Create document fragment for given html string
-function createDOMFragment(htmlStr) {
-    var frag = document.createDocumentFragment(),
-        temp = document.createElement('div');
-    temp.innerHTML = htmlStr;
-    while (temp.firstChild) {
-        frag.appendChild(temp.firstChild);
-    }
-    return frag;
 }
 
 // Remove all items in the grid
@@ -68,6 +92,31 @@ function removeItems() {
     items.forEach(item => {
         gridElement.removeChild(item);
     })
+}
+
+/* Remove all items within the selectedElements array. 
+   TODO: selectedElements is a global variable. Can we make this safer?
+*/
+function removeSelectedItems() {
+    grid.remove(selectedElements);
+    selectedElements.forEach(element => {
+        gridElement.removeChild(element);
+    })
+    selectedElements = [];
+}
+
+// Build the GridData object and convert it to a JSON string
+function generateJSON() {
+    let title = titleInput.value;
+    let titleVisible = isTitleVisible();
+    let layout = new Layout(new GridSize(3, 2));
+    let widgets = [];
+
+    var gridData = new GridData(
+        titleInput.value, titleVisible, layout, widgets);
+
+    var jsonString = JSON.stringify(gridData)
+    jsonLabel.textContent = jsonString;
 }
 
 // Add selected item to array and show that it is selected
@@ -90,13 +139,19 @@ function toggleSelectElement(event) {
     })
 }
 
-/* Remove all items within the selectedElements array. 
-   TODO: selectedElements is a global variable. Can we make this safer?
-*/
-function removeSelectedItems() {
-    grid.remove(selectedElements);
-    selectedElements.forEach(element => {
-        gridElement.removeChild(element);
-    })
-    selectedElements = [];
+/* Helpers */
+
+function isTitleVisible() {
+    return titleVisibleToggle.textContent === "Set Hidden"
+}
+
+// Create document fragment for given html string
+function createDOMFragment(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
 }
